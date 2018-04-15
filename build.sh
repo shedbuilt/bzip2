c4db4b9b3f042ee4de35e2214c439eb1336d9463
Auston Stewart <auston@shedbuilt.net>
2018-04-15 09:36:52 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
-        make -j $SHED_NUMJOBS && \
-        make PREFIX="${SHED_FAKEROOT}/tools" install || exit 1
-        rm -v "${SHED_FAKEROOT}"/tools/bin/{bzcmp,bzegrep,bzfgrep,bzless}
-        ln -sv bzdiff "${SHED_FAKEROOT}/tools/bin/bzcmp"
-        ln -sv bzgrep "${SHED_FAKEROOT}/tools/bin/bzegrep"
-        ln -sv bzgrep "${SHED_FAKEROOT}/tools/bin/bzfgrep"
-        ln -sv bzmore "${SHED_FAKEROOT}/tools/bin/bzless"
+        make -j $SHED_NUM_JOBS && \
+        make PREFIX="${SHED_FAKE_ROOT}/tools" install || exit 1
+        rm -v "${SHED_FAKE_ROOT}"/tools/bin/{bzcmp,bzegrep,bzfgrep,bzless}
+        ln -sv bzdiff "${SHED_FAKE_ROOT}/tools/bin/bzcmp"
+        ln -sv bzgrep "${SHED_FAKE_ROOT}/tools/bin/bzegrep"
+        ln -sv bzgrep "${SHED_FAKE_ROOT}/tools/bin/bzfgrep"
+        ln -sv bzmore "${SHED_FAKE_ROOT}/tools/bin/bzless"
         ;;
     *)
-        patch -Np1 -i "${SHED_PATCHDIR}/bzip2-1.0.6-install_docs-1.patch"
+        patch -Np1 -i "${SHED_PKG_PATCH_DIR}/bzip2-1.0.6-install_docs-1.patch"
         sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
         sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
-        make -j $SHED_NUMJOBS -f Makefile-libbz2_so || return 1
+        make -j $SHED_NUM_JOBS -f Makefile-libbz2_so || return 1
         make clean
-        make -j $SHED_NUMJOBS || return 1
-        make PREFIX="${SHED_FAKEROOT}/usr" install || return 1
-        mkdir -pv "${SHED_FAKEROOT}"/{bin,lib}
-        cp -v bzip2-shared "${SHED_FAKEROOT}/bin/bzip2"
-        cp -av libbz2.so* "${SHED_FAKEROOT}/lib"
-        ln -sv ../../lib/libbz2.so.1.0 "${SHED_FAKEROOT}/usr/lib/libbz2.so"
-        rm -v "${SHED_FAKEROOT}"/usr/bin/{bunzip2,bzcat,bzip2}
-        ln -sv bzip2 "${SHED_FAKEROOT}/bin/bunzip2"
-        ln -sv bzip2 "${SHED_FAKEROOT}/bin/bzcat"
+        make -j $SHED_NUM_JOBS || return 1
+        make PREFIX="${SHED_FAKE_ROOT}/usr" install || return 1
+        mkdir -pv "${SHED_FAKE_ROOT}"/{bin,lib}
+        cp -v bzip2-shared "${SHED_FAKE_ROOT}/bin/bzip2"
+        cp -av libbz2.so* "${SHED_FAKE_ROOT}/lib"
+        ln -sv ../../lib/libbz2.so.1.0 "${SHED_FAKE_ROOT}/usr/lib/libbz2.so"
+        rm -v "${SHED_FAKE_ROOT}"/usr/bin/{bunzip2,bzcat,bzip2}
+        ln -sv bzip2 "${SHED_FAKE_ROOT}/bin/bunzip2"
+        ln -sv bzip2 "${SHED_FAKE_ROOT}/bin/bzcat"
         ;;
 esac
